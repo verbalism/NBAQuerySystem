e87ca57b4295168735554ba28f7450b3185b630e
T^T <1308040231@qq.com>
2015-03-13 14:55:53 +0800

    IService

--- coding/iteration1/src/businesslogic/IService.java
+++ coding/iteration1/src/businesslogic/IService.java
@@ -3,13 +3,11 @@ package businesslogic;
 import java.util.ArrayList;
 
 import vo.playerCondition;
-<<<<<<< HEAD
+
 import vo.playerInfoVO;
-import vo.teamInfo;
-=======
-import vo.playerInfo;
+
 import vo.teamInfoVO;
->>>>>>> 579efd6e74d948d617ed568517e11a9c4b13685c
+
 
 public interface IService {
 	public teamInfoVO getSingleTeamInfo(teamInfoVO t);
--- coding/iteration1/src/vo/teamInfoVO.java
+++ coding/iteration1/src/vo/teamInfoVO.java
@@ -1,38 +1,217 @@
 package vo;
 
 public class teamInfoVO {
-	String teamname;//球队名称
-	int gamesplayed;//比赛场数
-	int fieldGoalsMade;//投篮命中数
-	int fieldGoalsAttempted;//投篮出手数
-	int threePointFieldGoalsMade;//三分命中数
-	int threePointFieldGoalsAttempted;//三分出手数
-	int freeThrowsMade;//罚球命中数
-	int freeThrowsAttempted;//罚球出手数
-	int offensiveRebounds;//进攻篮板数
-	int defensiveRebounds;//防守篮板数
-	int rebounds;//篮板数
-	int assists;//助攻数
-	int steals;//抢断数
-	int blocks;//盖帽数
-	int turnovers;//失误数
-	int fouls;//犯规数
-	int points;//失误数
-	double fieldGoalPercentage;//投篮命中率
-	double threePointFieldGoalPercentage;//三分命中率
-	double freeThrowPercentage;//罚球命中率
-	double winPercentage;//胜率
-	double possessions;//进攻回合
-	double offensiveRating;//进攻效率
-	double defensiveRating;//防守效率
-	double offensiveReboundPercentage;//进攻篮板效率
-	double defensiveReboundPercentage;//防守篮板效率
-	double stealPercentage;//抢断效率
-	double assisyPercentage;//助攻率
+	private String teamName;//球队名称
+	private int gamesPlayed;//比赛场数
+	private double fieldGoalsMade;//投篮命中数
+	private double fieldGoalsAttempted;//投篮出手数
+	private double threePointFieldGoalsMade;//三分命中数
+	private double threePointFieldGoalsAttempted;//三分出手数
+	private double freeThrowsMade;//罚球命中数
+	private double freeThrowsAttempted;//罚球出手数
+	private double offensiveRebounds;//进攻篮板数
+	private double defensiveRebounds;//防守篮板数
+	private double rebounds;//篮板数
+	private double assists;//助攻数
+	private double steals;//抢断数
+	private double blocks;//盖帽数
+	private double turnovers;//失误数
+	private double fouls;//犯规数
+	private double points;//失误数
+	private double fieldGoalPercentage;//投篮命中率
+	private double threePointFieldGoalPercentage;//三分命中率
+	private double freeThrowPercentage;//罚球命中率
+	private double winPercentage;//胜率
+	private double possessions;//进攻回合
+	private double offensiveRating;//进攻效率
+	private double defensiveRating;//防守效率
+	private double offensiveReboundPercentage;//进攻篮板效率
+	private double defensiveReboundPercentage;//防守篮板效率
+	private double stealPercentage;//抢断效率
+	private double assisyPercentage;//助攻率
+	
 	
 	public teamInfoVO(){
-
+		
 	}
+	
+	
+	public void setTeamName(String tn){
+		teamName=tn;
+	}
+	public String getTeamName(){
+		return teamName;
+	}
+	
+	public void setgamesPlayed(int gp){
+		gamesPlayed=gp;
+	}
+	public int getgamesPlayed(){
+		return gamesPlayed;
+	}
+	
+	public double getFieldGoalsMade() {
+		return fieldGoalsMade;
+	}
+	public void setFieldGoalsMade(double fieldGoalsMade) {
+		this.fieldGoalsMade = fieldGoalsMade;
+	}
+		
+	public double getFieldGoalsAttempted() {
+		return fieldGoalsAttempted;
+	}
+	public void setFieldGoalsAttempted(double fieldGoalsAttempted) {
+		this.fieldGoalsAttempted = fieldGoalsAttempted;
+	}
+	public double getThreePointFieldGoalsMade() {
+		return threePointFieldGoalsMade;
+	}
+	public void setThreePointFieldGoalsMade(double threePointFieldGoalsMade) {
+		this.threePointFieldGoalsMade = threePointFieldGoalsMade;
+	}
+	public double getThreePointFieldGoalsAttempted() {
+		return threePointFieldGoalsAttempted;
+	}
+	public void setThreePointFieldGoalsAttempted(
+			double threePointFieldGoalsAttempted) {
+		this.threePointFieldGoalsAttempted = threePointFieldGoalsAttempted;
+	}
+	public double getFreeThrowsMade() {
+		return freeThrowsMade;
+	}
+	public void setFreeThrowsMade(double freeThrowsMade) {
+		this.freeThrowsMade = freeThrowsMade;
+	}
+	public double getFreeThrowsAttempted() {
+		return freeThrowsAttempted;
+	}
+	public void setFreeThrowsAttempted(double freeThrowsAttempted) {
+		this.freeThrowsAttempted = freeThrowsAttempted;
+	}
+	public double getOffensiveRebounds() {
+		return offensiveRebounds;
+	}
+	public void setOffensiveRebounds(double offensiveRebounds) {
+		this.offensiveRebounds = offensiveRebounds;
+	}
+	public double getDefensiveRebounds() {
+		return defensiveRebounds;
+	}
+	public void setDefensiveRebounds(double defensiveRebounds) {
+		this.defensiveRebounds = defensiveRebounds;
+	}
+	public double getRebounds() {
+		return rebounds;
+	}
+	public void setRebounds(double rebounds) {
+		this.rebounds = rebounds;
+	}
+	public double getAssists() {
+		return assists;
+	}
+	public void setAssists(double assists) {
+		this.assists = assists;
+	}
+	public double getSteals() {
+		return steals;
+	}
+	public void setSteals(double steals) {
+		this.steals = steals;
+	}
+	public double getBlocks() {
+		return blocks;
+	}
+	public void setBlocks(double blocks) {
+		this.blocks = blocks;
+	}
+	public double getTurnovers() {
+		return turnovers;
+	}
+	public void setTurnovers(double turnovers) {
+		this.turnovers = turnovers;
+	}
+	public double getFouls() {
+		return fouls;
+	}
+	public void setFouls(double fouls) {
+		this.fouls = fouls;
+	}
+	public double getPoints() {
+		return points;
+	}
+	public void setPoints(double points) {
+		this.points = points;
+	}
+	public double getFieldGoalPercentage() {
+		return fieldGoalPercentage;
+	}
+	public void setFieldGoalPercentage(double fieldGoalPercentage) {
+		this.fieldGoalPercentage = fieldGoalPercentage;
+	}
+	public double getThreePointFieldGoalPercentage() {
+		return threePointFieldGoalPercentage;
+	}
+	public void setThreePointFieldGoalPercentage(
+			double threePointFieldGoalPercentage) {
+		this.threePointFieldGoalPercentage = threePointFieldGoalPercentage;
+	}
+	public double getFreeThrowPercentage() {
+		return freeThrowPercentage;
+	}
+	public void setFreeThrowPercentage(double freeThrowPercentage) {
+		this.freeThrowPercentage = freeThrowPercentage;
+	}
+	public double getWinPercentage() {
+		return winPercentage;
+	}
+	public void setWinPercentage(double winPercentage) {
+		this.winPercentage = winPercentage;
+	}
+	public double getPossessions() {
+		return possessions;
+	}
+	public void setPossessions(double possessions) {
+		this.possessions = possessions;
+	}
+	public double getOffensiveRating() {
+		return offensiveRating;
+	}
+	public void setOffensiveRating(double offensiveRating) {
+		this.offensiveRating = offensiveRating;
+	}
+	public double getDefensiveRating() {
+		return defensiveRating;
+	}
+	public void setDefensiveRating(double defensiveRating) {
+		this.defensiveRating = defensiveRating;
+	}
+	public double getOffensiveReboundPercentage() {
+		return offensiveReboundPercentage;
+	}
+	public void setOffensiveReboundPercentage(double offensiveReboundPercentage) {
+		this.offensiveReboundPercentage = offensiveReboundPercentage;
+	}
+	public double getDefensiveReboundPercentage() {
+		return defensiveReboundPercentage;
+	}
+	public void setDefensiveReboundPercentage(double defensiveReboundPercentage) {
+		this.defensiveReboundPercentage = defensiveReboundPercentage;
+	}
+	public double getStealPercentage() {
+		return stealPercentage;
+	}
+	public void setStealPercentage(double stealPercentage) {
+		this.stealPercentage = stealPercentage;
+	}
+	public double getAssisyPercentage() {
+		return assisyPercentage;
+	}
+	public void setAssisyPercentage(double assisyPercentage) {
+		this.assisyPercentage = assisyPercentage;
+	}
+	
+	
+	
 	
 
 }
